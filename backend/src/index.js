27072a42602cb3722879b266127cdc3fb3c6e4bf
Gabriel Roas <gabriel.roas@hotmail.com>
2021-10-31 19:26:14 -0300

    updated index backend - added json and urlencoded limit size

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,8 +5,9 @@ require('./database');
 
 const app = express();
 
+app.use(express.json({limit: '50mb'})); // FOR UNLIMITED TEXT
+app.use(express.urlencoded({limit: '50mb'})); 
 app.use(cors())
-app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
