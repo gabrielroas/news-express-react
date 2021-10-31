78fb3ea50b057857384e17706657cf573a8b8ba6
Gabriel Roas <gabriel.roas@hotmail.com>
2021-10-30 22:57:41 -0300

    created users migration and news migration

---
+++ backend/src/database/migrations/20211030223836-create-news-migration.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('news', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      content: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      news_url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      thumb_url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('news');
+  }
+};
