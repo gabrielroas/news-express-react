7b75da8471002cb5fb937ecfbc468ad51904d859
Gabriel Roas <gabriel.roas@hotmail.com>
2021-10-30 22:58:03 -0300

    created jwt middleware

---
+++ backend/src/middlewares/jwt.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const jwt = require("jsonwebtoken");
+
+
+const verifyJWT = (req, res, next) => {
+    const token = req.headers['authorization'];
+
+    if (!token) return res.status(401).json({ error: 'Token not found / Empty token.' });
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(500).json({ error: 'Unauthenticated token.' });
+        }
+        req.userId = decoded.user;
+        next();
+    });
+}
+
+module.exports = verifyJWT;
