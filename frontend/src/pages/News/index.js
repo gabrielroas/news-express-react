98f559e413ce132998dbd496d7d9fa3c1439c0ae
Gabriel Roas <gabriel.roas@hotmail.com>
2021-11-02 18:27:32 -0300

    created News page - view specific News

--- frontend/src/pages/News/index.js
+++ frontend/src/pages/News/index.js
@@ -1,11 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Component } from "react";
+import api from "../../services/api";
 
-const News = (props) => {
-  return (
-    <Fragment>
-      <h1 className="f1">Titulo: {props.match.params.news_url}</h1>
-    </Fragment>
-  );
-};
+class News extends Component {
+  state = {
+    news: '',
+    author: ''
+  }
+
+  async componentDidMount() {
+    const response = await api.get(`news/view/${this.props.match.params.news_url}`)
+    this.setState({ news: response.data });
+    this.setState({ author: response.data.author.name })
+
+  }
+
+  render() {
+    const { news, author } = this.state;
+
+    const FormatDateCreatedAt = (unformattedDate) => {
+      var currentDate = new Date(unformattedDate);
+      var newDate =
+        ("00" + currentDate.getDate()).slice(-2) + "/" +
+        ("00" + (currentDate.getMonth() + 1)).slice(-2) + "/" +
+        currentDate.getFullYear() + " " +
+        ("00" + currentDate.getHours()).slice(-2) + ":" +
+        ("00" + currentDate.getMinutes()).slice(-2) + ":" +
+        ("00" + currentDate.getSeconds()).slice(-2);
+      return newDate;
+    }
+    const createdAt = FormatDateCreatedAt(news.createdAt);
+
+    return (
+      <div>
+        <li key={news.id}>
+          <h4>
+            {news.title}
+          </h4>
+          {createdAt}
+          <div dangerouslySetInnerHTML={{ __html: news.content }}></div>
+          Autor: {author}
+          <img src={news.thumb_url} alt="Thumbnail" />
+          <br />
+        </li>
+
+      </div>
+    )
+  }
+}
 
 export default News;
