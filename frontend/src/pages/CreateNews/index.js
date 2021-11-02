e867605ae15260d5f20145d0aa513dcaa436bbf3
Gabriel Roas <gabriel.roas@hotmail.com>
2021-11-01 21:40:59 -0300

    created CreateNews page - added index and style files

---
+++ frontend/src/pages/CreateNews/index.js
@@ -0,0 +1,91 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+
+import Logo from "../../assets/newspaper-news-svgrepo-com.svg";
+import api from "../../services/api";
+import { Form, Container, Success, Error, CustomEditor } from "./styles";
+
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+class CreateNews extends Component {
+  state = {
+    title: "",
+    content: "",
+    thumb_url: "",
+    error: "",
+    success: ""
+  };
+
+  handleCreateNews = async e => {
+    e.preventDefault();
+    const { title, content, thumb_url } = this.state;
+    if (!title || !content) {
+      this.setState({ error: "Preencha o título e o conteúdo da notícia." });
+    } else {
+      try {
+        const response = await api.post("/news/create", { title, content, thumb_url });
+        this.setState({ success: "Notícia criada com sucesso! Vocẽ será redirecionado." });
+
+        setTimeout(function () {
+          window.location.href = "/";
+
+        }, 2000)
+      } catch (err) {
+        this.setState({
+          error:
+            "Houve um problema ao cadastrar a notícia, verifique os campos preenchidos."
+        });
+      }
+    }
+  };
+
+
+  render() {
+    return (
+      <Container>
+        <Form onSubmit={this.handleCreateNews}>
+          <img src={Logo} alt="News" />
+          Nova Notícia
+          <hr />
+          {this.state.error && <Error>{this.state.error}</Error>}
+          {this.state.success && <Success>{this.state.success}</Success>}
+          <input
+            type="text"
+            placeholder="Título da notícia *"
+            onChange={e => this.setState({ title: e.target.value })}
+          />
+          <CustomEditor><CKEditor
+            editor={ClassicEditor}
+            data=""
+            onReady={editor => {
+              // You can store the "editor" and use when it is needed.
+              console.log('Editor is ready to use!', editor);
+            }}
+            onChange={(event, editor) => {
+              const data = editor.getData();
+              this.setState({ content: data })
+            }}
+            onBlur={(event, editor) => {
+              console.log('Blur.', editor);
+            }}
+            onFocus={(event, editor) => {
+              console.log('Focus.', editor);
+            }}
+          /></CustomEditor>
+
+          <input
+            type="text"
+            placeholder="Insira o link da Thumbnail"
+            onChange={e => this.setState({ thumb_url: e.target.value })}
+          />
+          <button type="submit"><FontAwesomeIcon icon={faPlus} /> Criar notícia</button>
+        </Form>
+      </Container>
+    );
+  }
+}
+
+export default withRouter(CreateNews);
---
+++ frontend/src/pages/CreateNews/styles.js
@@ -0,0 +1,87 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  padding: 60px;
+
+`;
+
+export const Form = styled.form`
+  width: 70%;
+  background: #fff;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  img {
+    width: 100px;
+    margin: 10px 0 40px;
+  }
+
+  input {
+    // flex: 1;
+    border-radius: 5px;
+    height: 46px;    
+    margin-bottom: 8px;
+    padding: 0 20px;
+    font-size: 15px;
+    width: 100%;  
+    border: 2px solid #ddd;
+    &:focus {
+    border: 1px solid #1b74fb;
+    }
+  }
+  
+  button {
+    color: #fff;
+    font-size: 16px;
+    background: #63a0fc;
+    height: 56px;
+    border: 0;
+    border-radius: 5px;
+    width: 25%;
+  }
+  hr {
+    margin: 20px 0;
+    border: none;
+    border-bottom: 1px solid #cdcdcd;
+    width: 100%;
+  }
+  a {
+    font-size: 16;
+    font-weight: bold;
+    color: #999;
+    text-decoration: none;
+  }
+`;
+
+export const Success = styled.p`
+  background: #d8ffbf;
+  color: #00810b;
+  margin-bottom: 15px;
+  border: 1px solid #228900;
+  padding: 10px;
+  width: 100%;
+  text-align: center;
+`;
+
+export const Error = styled.p`
+  color: #ff3333;
+  margin-bottom: 15px;
+  border: 1px solid #ff3333;
+  padding: 10px;
+  width: 100%;
+  text-align: center;
+`;
+
+export const CustomEditor = styled.p`
+  width: 100%; 
+  weght: 200px;
+  text-align: center;
+  border-radius: 5px;  
+  margin-bottom: 8px;  
+  border: 0.5px solid #ddd;
+`;
