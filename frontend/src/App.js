000974fe831af8d59012aa2ced1e3fc26c37835d
Gabriel Roas <gabriel.roas@hotmail.com>
2021-11-01 21:40:23 -0300

    updated App file - added Header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,12 @@
 import React, { Fragment } from 'react';
 import Routes from './routes'
 import GlobalStyle from './styles/global'
-import { logout, isAuthenticated } from "./services/auth";
+import Header from "./components/Header";
 
 const App = () => (
 
     <Fragment>
-        <h1>News</h1>
-        <a href="/home"> Home </a>|
-        <a href="/profile"> MyProfile </a>|
-        {
-            isAuthenticated() ?
-            <a href="/" onClick={() => logout()}> LogOut </a> :
-            <a href="/signin"> Login</a>
-        }
-
+        <Header />
         <Routes />
         <GlobalStyle />
     </Fragment>
