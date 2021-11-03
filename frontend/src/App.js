6b0abd1d44a31610cc72dc43b58ff66737eab5cf
Gabriel Roas <gabriel.roas@hotmail.com>
2021-11-03 14:57:08 -0300

    created Footer component and added on App index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React, { Fragment } from 'react';
 import Routes from './routes'
 import GlobalStyle from './styles/global'
 import Header from "./components/Header";
+import Footer from './components/Footer';
 
 const App = () => (
 
     <Fragment>
         <Header />
         <Routes />
+        <Footer />
         <GlobalStyle />
     </Fragment>
 )
