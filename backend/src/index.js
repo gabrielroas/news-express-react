d56db8a95d1ce8a72937fa089810fe356bbbb71b
Gabriel Roas <gabriel.roas@hotmail.com>
2021-11-03 04:21:54 -0300

    updated index backend - added hostname and routes console.log

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,15 @@ require('./database');
 
 const app = express();
 
+const hostname = '127.0.0.1';
+const port = 3333;
+
 app.use(express.json({limit: '50mb'})); // FOR UNLIMITED TEXT
 app.use(express.urlencoded({limit: '50mb'})); 
-app.use(cors())
-app.use(routes);
 
-app.listen(3333);
+app.use(cors())
+
+app.use(routes);
+app.listen(port, hostname, () => {
+    console.log(`🏁 Server running at http://${hostname}:${port}/`);
+  });
